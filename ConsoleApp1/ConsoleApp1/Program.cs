5fdac01ae9c048b78ce426dc828902f8b570c9da
Stone Wen <Stone.Wen@lumentum.com>
2019-05-22 13:50:45 +0800

    modiy

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //add comment
-            Console.WriteLine("this is master branch");
+            Console.WriteLine("this is master branch!");
         }
     }
 }
